14cba11a1c09c322eef5b93ae8ef07a63894692f
Keshav756 <144075944+Keshav756@users.noreply.github.com>
2025-08-25 22:42:02 +0530

    Update errorMiddlewares.js

--- server/middlewares/errorMiddlewares.js
+++ server/middlewares/errorMiddlewares.js
@@ -1,42 +1,41 @@
 class ErrorHandler extends Error {
-    constructor(message,StatusCode) {
+    constructor(message, statusCode) {
         super(message);
-        this.statusCode = StatusCode;
-
+        this.statusCode = statusCode;
     }
 }
 
-
+// Central error handling middleware
 export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
 
-    if(err.code === 11000){
-        const statusCode = 400;
-        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
-        err = new ErrorHandler(message,statusCode);
-    }
-    if(err.name === "JsonWebTokenError"){
-        const statusCode = 400;
-        const message = "Json Web Token is invalid. Try again";
-        err = new ErrorHandler(message,statusCode);
-    }
-    if(err.name === "TokenExpiredError"){
-        const statusCode = 400;
-        const message = "Json Web Token is expired. Try again";
-        err = new ErrorHandler(message,statusCode);
-    }
-    if(err.name === "CastError"){
-        const statusCode = 400;
-        const message = `Resource not found. Invalid: ${err.path}`;
-        err = new ErrorHandler(message,statusCode);
+    // Duplicate key error (MongoDB)
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered.`;
+        err = new ErrorHandler(message, 400);
     }
 
-    const errorMessage = err.errors ? Object.values(err.errors).map(error => error.message).join(' ') : err.message;
+    // JWT errors
+    if (err.name === "JsonWebTokenError") {
+        err = new ErrorHandler("Json Web Token is invalid. Try again.", 401);
+    }
+    if (err.name === "TokenExpiredError") {
+        err = new ErrorHandler("Json Web Token is expired. Try again.", 401);
+    }
+
+    // Invalid MongoDB ObjectId
+    if (err.name === "CastError") {
+        err = new ErrorHandler(`Resource not found. Invalid: ${err.path}`, 400);
+    }
+
+    const errorMessage = err.errors
+        ? Object.values(err.errors).map((error) => error.message).join(" ")
+        : err.message;
 
     return res.status(err.statusCode).json({
-        success : false,
-        message : errorMessage,
+        success: false,
+        message: errorMessage,
     });
 };
 
