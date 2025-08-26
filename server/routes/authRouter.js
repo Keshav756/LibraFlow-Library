86bc83f19963f941dd9c350cc9ef682427f0d350
Keshav756 <144075944+Keshav756@users.noreply.github.com>
2025-08-26 12:46:45 +0530

    Update authRouter.js

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,12 +4,13 @@ import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/register",register);
-router.post("/verify-otp",verifyOTP);
-router.post("/login",login);
-router.get("/logout", isAuthenticated ,logout);
+router.post("/register", register);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
 router.get("/me", isAuthenticated, getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
 router.put("/password/update", isAuthenticated, updatePassword);
-export default router; 
+
+export default router;
